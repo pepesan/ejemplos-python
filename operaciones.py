811c68d7f2f2a110b41dfba87f9f2dba15ffe390
pepesan <pepesan@gmail.com>
2017-06-22 13:17:23 +0200

    Initial commit

---
+++ operaciones.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print (2 + 4)
+print (6 - 4)
+print (6 * 3)
+print (6 / 3)
+print (6 % 3)
+print (6 // 3) # floor (integer) division: always truncates fractional remainders
+print (-5)
+print (3**2)   # three to the power of 2
