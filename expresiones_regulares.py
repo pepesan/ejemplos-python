d15028e4088d63e7320d8494717bfb3bef72f1e5
pepesan <pepesan@gmail.com>
2018-04-18 11:55:04 +0200

    mejoras en el ejemplo de expresiones regulares

--- expresiones_regulares.py
+++ expresiones_regulares.py
@@ -10,11 +10,15 @@ if re.search("cat","A cat and a rat can't be friends."):
 else:
     print ("No cat has been found :-(")
 
-if re.search("c[aeuio]t","A cat and a rat can't be friends."):
+if re.search("c[aeuio]t","A cpt and a rat can't be friends."):
     print ("Some kind of c[vocal]t has been found :-)")
 else:
     print ("No c[vocal]t has been found :-(")
 
+if re.search("[iI][pP][sS][uU][mM]","Lorem IpSum  blah blah blah"):
+    print ("Ha encontrado el ipsum con mayúscula o minúscula :-): ")
+else:
+    print ("Va a ser que no está :-(")
 
 
 
