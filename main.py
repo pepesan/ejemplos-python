311cb82d76c9dc65c44e5c3d43cd0969be411e8e
pepesan <pepesan@gmail.com>
2019-10-01 10:37:06 +0200

    Update main.py
    qjuitados nombres de alumnos

--- main.py
+++ main.py
@@ -5,9 +5,3 @@ Autor: David Vaquero <pepesan@gmail.com>
 """
 # HolaMundo
 print("Hello World!")
-print("Hola Mundo!")
-print("Hello Beatriz!")
-print("Hello María!")
-print("Hello Jokin!")
-print("Hello Roberto!")
-print("Hello Iñigo de Montoya, Tú mataste a mi padre, Prepárate a morir!")
