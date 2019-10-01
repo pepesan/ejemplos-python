c0e800b787aab96ba5f4dd9a0871ac7147a5aebd
pepesan <pepesan@gmail.com>
2019-10-01 10:34:41 +0200

    nombres de alumnos

--- main.py
+++ main.py
@@ -3,5 +3,11 @@
 Fichero principal de python
 Autor: David Vaquero <pepesan@gmail.com>
 """
-#HolaMundo
-print ("Hello World!")
+# HolaMundo
+print("Hello World!")
+print("Hola Mundo!")
+print("Hello Beatriz!")
+print("Hello María!")
+print("Hello Jokin!")
+print("Hello Roberto!")
+print("Hello Iñigo de Montoya, Tú mataste a mi padre, Prepárate a morir!")
