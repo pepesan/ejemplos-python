6d93eafe00d4711bd5d2ff5ae413521fd43ed468
pepesan <pepesan@gmail.com>
2019-10-03 10:07:47 +0200

    mejora del ejemplo de comparadores

--- comparaciones.py
+++ comparaciones.py
@@ -3,44 +3,60 @@
 a = 21
 b = 10
 
+# Sentencia condicional, con parte del if y parte del else
+# Comparamos dos valores, si a es igual a b
 if ( a == b ):
-   print ("a es igual a b")
+   print ("Comparacion 1: a es igual a b")
 else:
-   print ("Line 1 - a is not equal to b")
+   print ("Comparacion 1: a y b no son iguales")
 
+# Comparamos si son distintos valores
 if ( a != b ):
-   print ("Line 2 - a is not equal to b")
+   print ("Comparacion 2: a es distinto de b")
 else:
-   print ("Line 2 - a is equal to b")
+   print ("Comparacion 2: a y b son iguales")
 
+# Comparamos si a es menor que b
 if ( a < b ):
-   print ("Line 3 - a is less than b" )
+   print ("Comparacion 3:  a es menor que b" )
 else:
-   print ("Line 3 - a is not less than b")
+   print ("Comparacion 3: a no es menor que b")
 
+# Comparamos si a es mayor que b
 if ( a > b ):
-   print ("Line 4 - a is greater than b")
+   print ("Comparacion 4: a es mayor que b")
 else:
-   print ("Line 4 - a is not greater than b")
+   print ("Comparacion 4: a no es mayor que b")
 
-a,b=b,a #values of a and b swapped. a becomes 10, b becomes 21
-
+# Comparamos si a es menor o igual que b
 if ( a <= b ):
-   print ("Line 5 - a is either less than or equal to  b")
+   print ("Comparacion 5: a es menor o igual que b")
 else:
-   print ("Line 5 - a is neither less than nor equal to  b")
+   print ("Comparacion 5: a no es ni menor ni igual a b, entonces a es mayor que b")
 
+# Comparando si b es mayor o igual que a
 if ( b >= a ):
-   print ("Line 6 - b is either greater than  or equal to b")
+   print ("Comparacion 6:  b es mayor o igual que a")
 else:
-   print ("Line 6 - b is neither greater than  nor equal to b")
+   print ("Comparacion 6: b no es ni mayor ni igual a a, b es menor que a")
 
-if ( b is a ):
-   print ("b es a")
-else:
-   print ("b no es a")
+a = 21
+b = 21.0
+# b = "21" # en PHP, pero no en Python
 
-if ( b is not a ):
-   print ("b no es a")
+# Comparamos dos valores, si a es igual a b
+if ( a == b ):
+   print ("Comparacion 7: a es igual a b")
 else:
-   print ("b es a")
+   print ("Comparacion 7: a y b no son iguales")
+
+# Comparamos si b es idéntico a a
+if ( a is b ):
+   print ("Comparacion 8: a es b")
+else:
+   print ("Comparacion 8: a no es b")
+#Comparamos si a no es b, identidad
+if ( a is not b ):
+   print ("Comparacion 9: a no es b")
+else:
+   print ("Comparacion 9: a es b")
