b4e7043144466608155d344a4c1b1c871e662641
David Vaquero <pepesan@gmail.com>
2022-03-22 10:12:11 +0100

    mejora del ejemplo de funciones

--- funciones.py
+++ funciones.py
@@ -3,47 +3,52 @@
 def procedimiento():
     print("procedimiento")
 
+
 procedimiento()
+
 
 def suma(x, y):
     """Devuelve x + y"""
-    print("x:",x)
+    print("x:", x)
     print("y:", y)
     return x + y
 
-suma(4,7)
+
+suma(4, 7)
 # res = 5
-res=suma(2,3)
-print("Resultado: ",res)
+res = suma(2, 3)
+print("Resultado: ", res)
 
-res=suma(4,9)
+res = suma(4, 9)
 print(res)
 
-res=suma (2,y=3)
+res = suma(2, y=3)
 print(res)
 
-res=suma (x=2,y=3)
+res = suma(x=2, y=3)
 print(res)
 
-res=suma (y=3,x=2)
+res = suma(y=3, x=2)
 print(res)
-
 
 
 def resta(x, y=1):
     """parámetro opcional"""
     return x - y
 
+
 # x=5, y=1
-res=resta(5)
+res = resta(5)
 print(res)
-res=resta(5,2)
+res = resta(5, 2)
 print(res)
-res=resta(y=5,x=2)
+res = resta(y=5, x=2)
 print(res)
 
-def restaTres(x=0,y=0,z=0):
-    return x-y-z
+
+def restaTres(x=0, y=0, z=0):
+    return x - y - z
+
 
 # x=0, y=0,z=0
 print(restaTres())
@@ -53,32 +58,32 @@ print(restaTres(2))
 print(restaTres(2, 3))
 # x=2, y=3,z=4
 print(restaTres(2, 3, 4))
-#y=2
+# y=2
 print(restaTres(y=2))
-#z=2
+# z=2
 print(restaTres(z=2))
 
 
+def multi(x=2, y=3):
+    return x * y
 
-def multi(x=2,y=3):
-    return x*y
 
 # x=2 , y=3
-res=multi()
+res = multi()
 print(res)
 # x=4 , y=3
-res=multi(4)
+res = multi(4)
 print(res)
 # x=4 , y=5
-res=multi(4,5)
+res = multi(4, 5)
 print(res)
 
 # x=3 , y=5
-res=multi(3,5)
+res = multi(3, 5)
 print(res)
 
 # x=2 , y=3
-res=multi(y=3)
+res = multi(y=3)
 print(res)
 
 
@@ -86,9 +91,11 @@ print(res)
 def a(c=0):
     return 100 + c
 
+
 # def b(n=120):
 def b(n=a(20)):
     return n + 50
+
 
 # imprime 70
 print(b(20))
@@ -97,61 +104,71 @@ print(b(20))
 print(b())
 
 
-
 def f(*args):
     return args
 
-print("Parámetros variable en número",f(1, 5, True, False, "Hello, world!"))
 
-print("Parámetros variable en número",f(1, 5))
+print("Parámetros variable en número", f(1, 5, True, False, "Hello, world!"))
+
+print("Parámetros variable en número", f(1, 5))
 
 
 def f(*args, **kwargs):
     return args, kwargs
 
+
 args, kwargs = f(True, False, 3.5, message="Hello, world!", year=2014)
 print(args)
-#(True, False, 3.5)
+# (True, False, 3.5)
 print(kwargs)
-#{'message': 'Hello, world!', 'year': 2014}
+
+
+# {'message': 'Hello, world!', 'year': 2014}
 
 
 def factorial(n):
     if n == 1:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n - 1)
+
 
 print(factorial(2))
+
 
 def factorial(n):
     print("factorial has been called with n = " + str(n))
     if n == 1:
         return 1
     else:
-        res = n * factorial(n-1)
-        print("intermediate result for ", n, " * factorial(" ,n-1, "): ",res)
+        res = n * factorial(n - 1)
+        print("intermediate result for ", n, " * factorial(", n - 1, "): ", res)
         return res
+
 
 print(factorial(5))
 
 
 def sumados(x):
-    x = x+2
+    x = x + 2
     return x
-res=sumados(3)
-print("resultado:",res)
-x=3
-res= sumados(x)
-print("X:",x)
-print("resultado:",res)
+
+
+res = sumados(3)
+print("resultado:", res)
+x = 3
+res = sumados(x)
+print("X:", x)
+print("resultado:", res)
+
 
 def restados(x):
-    x = x-2
+    x = x - 2
 
-res= restados(3)
+
+res = restados(3)
 print("Res:", res)
 
-print("Resta:",restados(4))
+print("Resta:", restados(4))
 
-print("Resta:",restados(7))
+print("Resta:", restados(7))
