cf3933d3f26c73f7ec16dce9bc38bdb93fe6d717
David Vaquero <pepesan@gmail.com>
2022-03-22 12:30:40 +0100

    corrección ejercicio clases repaso

--- ejercicios/clases2.py
+++ ejercicios/clases2.py
@@ -1,28 +1,43 @@
 class Persona:
-    def __init__(self,nombre="",dni="", tlf="",email="", direccion="",direcciones=[]):
-        self.nombre=nombre
-        self.dni=dni
-        self.tlf=tlf
-        self.email=email
-        self.direcciones=direcciones
-        self.setDireccion(direccion)
-    def setDireccion(self,direccion):
-        self.direccion=direccion
+    def __init__(self, nombre="", dni="", tlf="", email="", direccion="", direcciones=None):
+        if direcciones is None:
+            direcciones = []
+        self.nombre = nombre
+        self.dni = dni
+        self.tlf = tlf
+        self.email = email
+        self.direccion = direccion
+        if direcciones is not None:
+            self.direcciones = direcciones
+        else:
+            self.direcciones = []
+        if self.direccion != "":
+            self.setDireccion(direccion)
+
+    def setDireccion(self, direccion):
+        self.direccion = direccion
         self.direcciones.append(self.direccion)
 
+
 pepito = Persona()
-pepito.nombre="Pepito"
-pepito.dni="07977865A"
-pepito.tlf="945123456"
-pepito.email="pepito@gmail.com"
+pepito.nombre = "Pepito"
+pepito.dni = "07977865A"
+pepito.tlf = "945123456"
+pepito.email = "pepito@gmail.com"
 pepito.setDireccion("Castrourdiales 10")
 print(pepito.direccion)
+print(pepito.direcciones)
 
-juanita= Persona(nombre="Juanita")
+juanita = Persona(nombre="Juanita")
 print(juanita.nombre)
-juanita= Persona(dni="07888997A")
+juanita = Persona(dni="07888997A")
 print(juanita.dni)
-juanita= Persona("Juanita", "07998877A")
+juanita = Persona("Juanita", "07998877A")
 print(juanita.nombre)
 print(juanita.dni)
 print(juanita)
+print(juanita.direcciones)
+juanita.setDireccion("Una")
+print(juanita.direcciones)
+juanita.setDireccion("Dos")
+print(juanita.direcciones)
